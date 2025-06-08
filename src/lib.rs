fc7ae6146b8a2b1c14a4e5b1ae28b77575a9e8a2
Matthias Jung <m.jung@uni-wuerzburg.de>
2025-06-08 23:03:28 +0200

    feature: first introduction of BitSet to make marking possible

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Bit {
     Zero,
@@ -5,9 +7,17 @@ pub enum Bit {
     DontCare,
 }
 
-pub fn hamming(a: &Vec<Bit>, b: &Vec<Bit>) -> Option<Vec<Bit>> {
-    a.iter()
-        .zip(b)
+#[derive(PartialEq, Debug)]
+pub struct BitSet {
+    pub bits: Vec<Bit>,
+    pub used: bool,
+}
+
+pub fn hamming(a: &BitSet, b: &BitSet) -> Option<BitSet> {
+    let bits = a
+        .bits
+        .iter()
+        .zip(b.bits.iter())
         .map(|(bit_a, bit_b)| match (bit_a, bit_b) {
             (Bit::Zero, Bit::One) | (Bit::One, Bit::Zero) => Some(Bit::DontCare),
             (Bit::Zero, Bit::Zero) => Some(Bit::Zero),
@@ -15,19 +25,70 @@ pub fn hamming(a: &Vec<Bit>, b: &Vec<Bit>) -> Option<Vec<Bit>> {
             (Bit::DontCare, Bit::DontCare) => Some(Bit::DontCare),
             _ => None,
         })
+        .collect::<Option<Vec<Bit>>>()?;
+
+    Some(BitSet { bits, used: false })
+}
+
+pub fn compare_sets(set_a: &[BitSet], set_b: &[BitSet]) -> Option<Vec<BitSet>> {
+    let result_set: Vec<_> = set_a
+        .iter()
+        .flat_map(|a| {
+            set_b.iter().filter_map(move |b| hamming(a, b)) // filters None out
+        })
+        .collect();
+
+    Some(result_set)
+}
+
+pub fn parse(bits: &[Vec<i32>]) -> Vec<Vec<Bit>> {
+    bits.iter()
+        .map(|bit_vec| {
+            bit_vec
+                .iter()
+                .map(|&b| if b == 1 { Bit::One } else { Bit::Zero })
+                .collect()
+        })
         .collect()
 }
 
-pub fn compare_sets(set_a: &Vec<Vec<Bit>>, set_b: &Vec<Vec<Bit>>) -> Option<Vec<Vec<Bit>>> {
-    // The sets set_a and set_b can have different lenght. Now we compare each element of set_a with each element of set_b, by calling the hamming function. If there is a match, we store the result in a new set. If it returns None, then we go to the next element of set_b or the next element of set_a.
-    let mut result_set = Vec::new();
+pub fn sort_bits(bits: &[Vec<Bit>]) -> Vec<Vec<BitSet>> {
+    let mut groups: HashMap<usize, Vec<Vec<Bit>>> = HashMap::new();
 
-    for a in set_a {
-        for b in set_b {
-            if let Some(result) = hamming(a, b) {
-                result_set.push(result);
-            }
+    for vec in bits {
+        let count = vec.iter().filter(|bit| **bit == Bit::One).count();
+        groups.entry(count).or_default().push(vec.clone());
+    }
+
+    // Now collect in sorted order
+    let mut result = Vec::new();
+    let mut keys: Vec<_> = groups.keys().copied().collect();
+    keys.sort(); // sort by the number of ones
+
+    for key in keys {
+        if let Some(group) = groups.remove(&key) {
+            let bitsets = group
+                .into_iter()
+                .map(|bits| BitSet { bits, used: false })
+                .collect();
+            result.push(bitsets);
         }
     }
-    Some(result_set)
+
+    result
+}
+
+pub fn find_prime_implicants(groups: Vec<Vec<BitSet>>) -> Vec<BitSet> {
+    let mut result = Vec::new();
+
+    for window in groups.windows(2) {
+        let a = &window[0];
+        let b = &window[1];
+
+        if let Some(comparison_result) = compare_sets(a, b) {
+            result.extend(comparison_result);
+        }
+    }
+
+    result
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,31 +1,140 @@
-use qmc::{Bit, compare_sets, hamming};
+use qmc::{Bit, BitSet, compare_sets, hamming, parse, sort_bits};
 
 #[cfg(test)]
 mod tests {
+    use qmc::find_prime_implicants;
+
     use super::*;
 
     #[test]
-    fn test_hamming() {
+    fn test_hamming_generate() {
         // Test the generation of a Dontcare:
-        let a = vec![Bit::Zero, Bit::One, Bit::Zero, Bit::DontCare];
-        let b = vec![Bit::Zero, Bit::Zero, Bit::Zero, Bit::DontCare];
-        let e: Vec<Bit> = vec![Bit::Zero, Bit::DontCare, Bit::Zero, Bit::DontCare];
+        let a = BitSet {
+            bits: vec![Bit::Zero, Bit::One, Bit::Zero, Bit::DontCare],
+            used: false,
+        };
+        let b = BitSet {
+            bits: vec![Bit::Zero, Bit::Zero, Bit::Zero, Bit::DontCare],
+            used: false,
+        };
+        let e = BitSet {
+            bits: vec![Bit::Zero, Bit::DontCare, Bit::Zero, Bit::DontCare],
+            used: false,
+        };
         assert_eq!(hamming(&a, &b), Some(e));
+    }
 
+    #[test]
+    fn test_hamming_ignore() {
         // Test the case where DontCares don't lign up:
-        let a = vec![Bit::Zero, Bit::One, Bit::Zero];
-        let b = vec![Bit::DontCare, Bit::One, Bit::Zero];
+        let a = BitSet {
+            bits: vec![Bit::Zero, Bit::One, Bit::Zero],
+            used: false,
+        };
+        let b = BitSet {
+            bits: vec![Bit::DontCare, Bit::One, Bit::Zero],
+            used: false,
+        };
         assert_eq!(hamming(&a, &b), None);
     }
 
     #[test]
     fn test_compare_sets() {
-        let set_a = vec![vec![Bit::Zero, Bit::Zero]];
-        let set_b = vec![vec![Bit::Zero, Bit::One], vec![Bit::One, Bit::Zero]];
+        let set_a = vec![BitSet {
+            bits: vec![Bit::Zero, Bit::Zero],
+            used: false,
+        }];
+        let set_b = vec![
+            BitSet {
+                bits: vec![Bit::Zero, Bit::One],
+                used: false,
+            },
+            BitSet {
+                bits: vec![Bit::One, Bit::Zero],
+                used: false,
+            },
+        ];
         let set_e = vec![
-            vec![Bit::Zero, Bit::DontCare],
-            vec![Bit::DontCare, Bit::Zero],
+            BitSet {
+                bits: vec![Bit::Zero, Bit::DontCare],
+                used: false,
+            },
+            BitSet {
+                bits: vec![Bit::DontCare, Bit::Zero],
+                used: false,
+            },
         ];
         assert_eq!(compare_sets(&set_a, &set_b), Some(set_e));
+    }
+
+    #[test]
+    fn test_parse() {
+        let bits = vec![
+            vec![0, 0, 0], //
+            vec![0, 0, 1], //
+            vec![1, 0, 0], //
+        ];
+
+        let expected = vec![
+            vec![Bit::Zero, Bit::Zero, Bit::Zero],
+            vec![Bit::Zero, Bit::Zero, Bit::One],
+            vec![Bit::One, Bit::Zero, Bit::Zero],
+        ];
+
+        assert_eq!(parse(&bits), expected);
+    }
+
+    #[test]
+    fn test_sort() {
+        let bits = vec![
+            vec![Bit::Zero, Bit::Zero, Bit::One],  //
+            vec![Bit::Zero, Bit::Zero, Bit::Zero], //
+            vec![Bit::One, Bit::Zero, Bit::Zero],  //
+            vec![Bit::One, Bit::Zero, Bit::One],   //
+        ];
+
+        let expected = vec![
+            vec![BitSet {
+                bits: vec![Bit::Zero, Bit::Zero, Bit::Zero],
+                used: false,
+            }],
+            vec![
+                BitSet {
+                    bits: vec![Bit::Zero, Bit::Zero, Bit::One],
+                    used: false,
+                },
+                BitSet {
+                    bits: vec![Bit::One, Bit::Zero, Bit::Zero],
+                    used: false,
+                },
+            ],
+            vec![BitSet {
+                bits: vec![Bit::One, Bit::Zero, Bit::One],
+                used: false,
+            }],
+        ];
+
+        assert_eq!(sort_bits(&bits), expected);
+    }
+
+    #[test]
+    fn test_find_prime_implicants() {
+        let bits = vec![
+            vec![BitSet {
+                bits: vec![Bit::Zero, Bit::Zero],
+                used: false,
+            }],
+            vec![BitSet {
+                bits: vec![Bit::Zero, Bit::One],
+                used: false,
+            }],
+        ];
+
+        let expected = vec![BitSet {
+            bits: vec![Bit::Zero, Bit::DontCare],
+            used: false,
+        }];
+
+        assert_eq!(find_prime_implicants(bits), expected);
     }
 }
